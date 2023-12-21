d732407076a962f15148f71f8fa0f54bf89e4675
Rar Developer <developer@raysandreach.com>
2023-12-21 15:25:20 +0530

    Create Test_Run.py

---
+++ Test_Run.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Create the main window (it won't be visible)
+root = tk.Tk()
+root.withdraw()
+
+# Display a pop-up message box
+messagebox.showinfo("Success", "This code runs successfully")
+
+# This line is optional but keeps the window from appearing
+root.destroy()
